262ffc4d93e76bade53ac5b4a12a48d058075fd6
VoHoangLong <vohoanglong07@gmail.com>
2015-07-31 11:36:51 +0700

    Aside in javascript

--- app.js
+++ app.js
@@ -56,18 +56,114 @@
 // a();
 
 
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms) {}
-    console.log("finished function");
+// function waitThreeSeconds() {
+//     var ms = 3000 + new Date().getTime();
+//     while (new Date() < ms) {}
+//     console.log("finished function");
+// }
+
+// function clickHandler() {
+//     console.log("click event !");
+// }
+
+// document.addEventListener('click', clickHandler);
+
+
+// waitThreeSeconds();
+// console.log("finished execution");
+
+//var a = 3 +4;
+//var a = 4 > 3;
+//var a = 3 + 4 * 5;
+//console.log(a);
+
+var a = (3 + 4) * 5;
+console.log(a);
+
+//var a = 2, b =3, c = 4;
+//a = b = c;
+//
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+//var a = 1 + 2;
+//var a = "Hello" + "world";
+//var a = "1" + 2 * 3;
+//console.log(a);
+//console.log( 3 < 2 < 1);
+
+console.log( 1 < 2 < 3);
+console.log(null < 1);
+console.log("" == 0);
+console.log("" == false);
+
+//var a = 0;
+
+//var b = false;
+
+//if (a === b) {
+//    console.log("They are equal");
+//} else {
+//    console.log("They are not equal");
+//}
+
+//var a;
+
+//a = 0;
+
+//goes to internet and look for a value
+//if (a || a === 0) {
+//    console.log("something is there");
+//}
+
+//function greet(name) {
+//    name = name || "<Your name here>";
+//    console.log("Hello " + name);
+//}
+//greet();
+//greet
+//greet("Jackie");
+
+//console.log(libraryName);
+/*var person = new Object();
+
+person["firstname"] = "Jackie";
+person["lastname"] = "Wu";
+var firstnameProperty = "firstname";
+console.log(person);
+console.log(person[firstnameProperty]);
+
+console.log(person.firstname);
+console.log(person.lastname);
+
+person.address = new Object();
+person.address.street = "Nguyen Van Dau";
+person.address.city = "Ho Chi Minh";
+
+console.log(person.address.city);
+console.log(person.address.street);
+console.log(person['address']["state"]);*/
+
+var person = {
+    firstname : "Jackie",
+    lastname : "Wu",
+    address : {
+        street : "Nguyen Van Dau",
+        city : "Ho Chi Minh",
+        country : "VietNam"
+    }
+};
+
+function greet(person) {
+    console.log("Hi " + person.firstname);
 }
 
-function clickHandler() {
-    console.log("click event !");
-}
+greet(person);
+greet({firstname: "Anna", lastname: "Joe"});
 
-document.addEventListener('click', clickHandler);
+console.log(person);
 
-
-waitThreeSeconds();
-console.log(finished execution);
+person.address2 = {
+    street: "Nguyen Dinh Chieu",
+};
